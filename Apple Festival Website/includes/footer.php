2665fff56da6831b2892b1bce78c390701e794c9
Anika Tasnim <71908209+anikasparkle22@users.noreply.github.com>
2022-03-31 22:12:28 -0400

    Add files via upload

---
+++ Apple Festival Website/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p><?php echo $source;?><cite><a href="https://www.downtownithaca.com/apple-harvest-festival/">Downtown Ithaca </a></cite></p>
+
+    <script type="text/javascript" src="public/scripts/jquery-3.6.0.js"></script>
+    <script type="text/javascript" src="public/scripts/drop-down.js"></script>
+  </footer>
