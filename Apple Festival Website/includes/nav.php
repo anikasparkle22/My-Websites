62fcba30eb5cdbe76e7e9ac9815daffbc5527e15
Anika Tasnim <71908209+anikasparkle22@users.noreply.github.com>
2022-03-31 22:12:13 -0400

    Create nav.php

---
+++ Apple Festival Website/includes/nav.php
@@ -0,0 +1,16 @@
+
+ <header>
+      <nav>
+        <a href="#menu" class="active" onclick="hm()"> ☰ </a>
+          <div id="avi" class="hidden">
+          <a href="/">About</a>
+            <a href="/festival-vendors">Market</a>
+            <a href="/entertainment">Entertainment</a>
+            <a href="/map-of-festival">Map</a>
+            <a href="/join-us">Join Us</a>
+        </div>
+      </nav>
+    </header>
+
+    <img src="public/images/hp1.png" alt="apple harvest festival logo" class= "head1">
+       <!-- Source: (original work) Anika Tasnim -->
